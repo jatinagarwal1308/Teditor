22ad75f3749068431d5a5af83dc31ed90f74de2e
Jatin Agarwal <aggarwaljatin16@gmail.com>
2019-10-07 17:14:44 +0530

    no message

---
+++ build/precache-manifest.1987483076b66d60d70a70d39afdf5ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa83218f13547959f015d870e20877f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54bfdbdc5bd30f62836f",
+    "url": "/static/css/main.93ef8613.chunk.css"
+  },
+  {
+    "revision": "47e68ff9a2d425131c31",
+    "url": "/static/js/2.7b5a87f2.chunk.js"
+  },
+  {
+    "revision": "54bfdbdc5bd30f62836f",
+    "url": "/static/js/main.a2c6058e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
